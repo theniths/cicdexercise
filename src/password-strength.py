ca4e6143b32399c366ce51c4c53a682bbf3fbe8f
nxvenk5 <v.niths@gmail.com>
2026-03-10 08:40:57 -0700

    update password check with comments lint fixes

--- src/password-strength.py
+++ src/password-strength.py
@@ -1,18 +1,17 @@
 import re
 
+
 def is_strong_password(password):
+    # Check for minimum length
+    if len(password) < 8:
+        return False
 
-   # Check for minimum length
-   if len(password) < 8:
-    return False
+    # Check for uppercase letter
+    if not re.search(r"[A-Z]", password):
+        return False
 
-   # Check for uppercase letter
-   if not re.search(r"[A-Z]", password):
-    return False
+    # Check for number in password
+    if not re.search(r"[0-9]", password):
+        return False
 
-   # Check for number in password
-   if not re.search(r"[0-9]", password):
-    return False
-
-   return True
-
+    return True
