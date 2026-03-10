1175ac880a9f71025a7012b983fd54bb94b9f515
nxvenk5 <v.niths@gmail.com>
2026-03-09 18:56:05 -0700

    password strength check

---
+++ src/password-strength.py
@@ -0,0 +1,14 @@
+import re
+
+def is_strong_password(password):
+   if len(password) < 8:
+       return False
+
+   if not re.search(r"[A-Z]", password):
+       return False
+
+   if not re.search(r"[0-9]", password):
+       return False
+
+   return True
+
