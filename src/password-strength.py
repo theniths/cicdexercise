1e756085307bcd2f5d951a3b8b16d5c3b462f609
nxvenk5 <v.niths@gmail.com>
2026-03-09 21:44:36 -0700

    update password check with comments

--- src/password-strength.py
+++ src/password-strength.py
@@ -1,12 +1,16 @@
 import re
 
 def is_strong_password(password):
+
+   # Check for minimum length
    if len(password) < 8:
     return False
 
+   # Check for uppercase letter
    if not re.search(r"[A-Z]", password):
     return False
 
+   # Check for number
    if not re.search(r"[0-9]", password):
     return False
 
