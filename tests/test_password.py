d72262682f0571e40c2e0a9f7f4cdd38ce86e255
nxvenk5 <v.niths@gmail.com>
2026-03-10 18:04:17 -0700

    Fixed test failures

---
+++ tests/test_password.py
@@ -0,0 +1,17 @@
+from src.password_strength import is_strong_password
+
+
+def test_strong_password():
+    assert is_strong_password("Secure123")
+
+
+def test_short_password():
+    assert not is_strong_password("abc")
+
+
+def test_no_number():
+    assert not is_strong_password("Password")
+
+
+def test_no_uppercase():
+    assert not is_strong_password("password123")
